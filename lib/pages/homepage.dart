d06b66dcc1b741b7ce9d8b2a72d38094bca0f9f5
EEngineerLoki <mtthwloki@gmail.com>
2024-07-03 22:54:46 +0800

    updated UI

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_translator/util/dialogbox.dart';
+import 'package:flutter_translator/util/speakbutton.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.white,
+              height: 300,
+              width: 400,
+              child: const Dialogbox(),
+            ),
+            const SizedBox(height: 20,)
+            ,
+            Container(
+              color: Colors.white,
+              height: 300,
+              width: 400,
+              child: const Dialogbox(),
+            ),
+            const SizedBox(height: 20,),
+            const SpeakButton()
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/util/dialogbox.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Dialogbox extends StatelessWidget {
+  const Dialogbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: TextFormField(
+              maxLines: 20,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+                hintText: 'Enter Text Here',
+                hintStyle: TextStyle(color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
